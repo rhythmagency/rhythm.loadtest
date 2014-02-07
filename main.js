c00cd20f2317557667383c64fddb3b2caee0e97a
Michael Lawrence <liquidplastik@gmail.com>
2014-02-07 00:32:15 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,32 @@
+var loadtest = require('loadtest'),
+	express = require('express'),
+	harp = require('harp'),
+	app = express(),
+	server,
+	io;
+
+app.configure(function () {
+	app.use(express.static(__dirname + '/public'));
+	app.use(harp.mount(__dirname + '/public'));
+});
+
+server = app.listen(9000);
+io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+	socket.on('start', function (options) {
+		console.log(options);
+
+		loadtest.loadTest(options, function (err, result) {
+			if (err) {
+				socket.emit('error', err);
+				console.error(err);
+			} else {
+				socket.emit('result', result);
+				console.log(result);
+			}
+		});
+	});
+});
+
+
